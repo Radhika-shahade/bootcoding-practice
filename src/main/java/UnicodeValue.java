66a50c27dc78392116cfa19830fd86b52907e6ba
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-15 18:28:42 +0530

    basic program practice

--- src/main/java/UnicodeValue.java
+++ src/main/java/UnicodeValue.java
@@ -4,6 +4,5 @@ public class UnicodeValue {
         System.out.println("INDIA");
         System.out.println("INDIA is known as Bharat in hindi");
         System.out.println("\u092D\u0964\u0931\u0924");
-
     }
 }
