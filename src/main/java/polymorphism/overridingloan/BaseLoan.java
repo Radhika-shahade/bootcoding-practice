c5be61ffcf7799327a440f54692a3c62ffc34410
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-19 16:18:15 +0530

    polymorphism example

---
+++ src/main/java/polymorphism/overridingloan/BaseLoan.java
@@ -0,0 +1,10 @@
+package polymorphism.overridingloan;
+
+public class BaseLoan {
+    public double calculateRoi()
+    {
+        System.out.println("parent method invoked");
+        return 10.50;
+    }
+
+}
