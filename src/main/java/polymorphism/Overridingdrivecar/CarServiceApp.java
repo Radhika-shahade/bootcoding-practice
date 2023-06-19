32db1b7d2455ead7fd483a67b36c9e8b193dbeb8
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-19 16:19:11 +0530

    polymorphism example

---
+++ src/main/java/polymorphism/Overridingdrivecar/CarServiceApp.java
@@ -0,0 +1,8 @@
+package polymorphism.Overridingdrivecar;
+
+public class CarServiceApp {
+    public static void main(String[] args) {
+        CarServiceChild csc= new CarServiceChild();
+        csc.drive(300,4,2);
+    }
+}
