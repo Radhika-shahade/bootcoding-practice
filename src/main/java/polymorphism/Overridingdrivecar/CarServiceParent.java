be750fab0fe32d4df9c3d2ab6534d55a5900908f
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-19 16:20:03 +0530

    polymorphism example

---
+++ src/main/java/polymorphism/Overridingdrivecar/CarServiceParent.java
@@ -0,0 +1,9 @@
+package polymorphism.Overridingdrivecar;
+
+public class CarServiceParent {
+    public void drive(double distance, int members, int stopCount)
+    {
+        System.out.println(distance + members + stopCount);
+    }
+
+}
