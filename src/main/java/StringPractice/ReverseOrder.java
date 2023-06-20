01a85ef511b8223ca8bd83162cfd2a65fdbc4767
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-20 21:30:25 +0530

    String practice

---
+++ src/main/java/StringPractice/ReverseOrder.java
@@ -0,0 +1,13 @@
+package StringPractice;
+
+public class ReverseOrder {
+    public static void main(String[] args) {
+        String s1=" Hello World ";
+      String s2[]= s1.split(" ");
+      int length= s2.length;
+        for(int i=length-1;i>=0;i--)
+        {
+            System.out.print(s2[i]);
+        }
+    }
+}
