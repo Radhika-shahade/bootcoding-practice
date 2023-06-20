33a4d93f7c8e3ba4a3b2138ca076b95d144eae8b
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-20 18:16:53 +0530

    String practice

---
+++ src/main/java/StringPractice/CountingCharacters.java
@@ -0,0 +1,9 @@
+package StringPractice;
+
+public class CountingCharacters {
+    public static void main(String[] args) {
+        String inputString="Hello world!";
+        int length=inputString.length();
+        System.out.println("Number of character are: " + length);
+    }
+}
