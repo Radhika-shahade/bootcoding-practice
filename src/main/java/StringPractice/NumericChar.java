c27ed3dbae62acb73a4379035bfef8a2668d6f66
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-20 21:29:57 +0530

    String practice

---
+++ src/main/java/StringPractice/NumericChar.java
@@ -0,0 +1,17 @@
+package StringPractice;
+//WAP to total number of numeric character present in a string
+public class NumericChar {
+    public static void main(String[] args) {
+        int count=0;
+        String s="34hdfsd45";
+        for(int i=0;i<s.length();i++)
+        {
+            char res= s.charAt(i);
+            if(Character.isDigit(res))
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
