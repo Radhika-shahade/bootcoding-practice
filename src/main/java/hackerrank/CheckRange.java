023316821b547743b40065e1df0b68882c6de7cf
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-20 18:15:31 +0530

    String practice

---
+++ src/main/java/hackerrank/CheckRange.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+public class CheckRange {
+    public static void main(String[] args) {
+        System.out.println("it takes min value: "+ Integer.MIN_VALUE);
+        System.out.println(" it takes max value: " + Integer.MAX_VALUE);
+        System.out.println("Int take types takes byte " + Integer.BYTES);
+        System.out.println("");
+        System.out.println("Float takes min value:" + Float.MIN_VALUE);
+        System.out.println("Float takes max value: "+ Float.MAX_VALUE);
+        System.out.println("Float takes byte " + Float.BYTES);
+        System.out.println("");
+        System.out.println("byte take min value:" + Byte.MIN_VALUE);
+        System.out.println("byte takes max value:"+ Byte.MAX_VALUE);
+        System.out.println("Byte takes bytes:" + Byte.BYTES);
+        System.out.println("");
+        System.out.println(" double take min value:"+ Double.MIN_VALUE);
+        System.out.println("double take max value:" + Double.MAX_VALUE);
+        System.out.println(" Double take byte:"+ Double.BYTES);
+    }
+}
