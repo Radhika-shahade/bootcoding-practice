ab6f05e89eb7d9d8bd321abbb58494ff7ef39c0d
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-21 18:06:56 +0530

    array practice

--- src/main/java/StringPractice/CountingCharacters.java
+++ src/main/java/StringPractice/CountingCharacters.java
@@ -6,4 +6,29 @@ public class CountingCharacters {
         int length=inputString.length();
         System.out.println("Number of character are: " + length);
     }
+
+    public static class ArrayContains {
+        public void findingSpecificElements(int a[])
+        {
+            int target=3;
+            for(int i=0;i<a.length;i++)
+            {
+                if(a[i]==target)
+                {
+                    System.out.println("array contains target element");
+                }
+    
+                }
+    
+            }
+    
+    
+        public static void main(String[] args) {
+            ArrayContains ac= new ArrayContains();
+            int b[]={6,4,5,6,7};
+            ac.findingSpecificElements(b);
+    
+        }
+    
+    }
 }
