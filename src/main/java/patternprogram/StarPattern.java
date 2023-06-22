9e640e310a18bce0350daa5eb7c92f1e92bec295
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-22 16:03:20 +0530

    Type casting

---
+++ src/main/java/patternprogram/StarPattern.java
@@ -0,0 +1,18 @@
+package patternprogram;
+//1
+//12
+//123
+//1234
+//12345
+public class StarPattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
