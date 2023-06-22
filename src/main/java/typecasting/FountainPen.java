d25ec9db712845fd12bb96511f474d8680fb73a8
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-22 15:56:23 +0530

    Type casting

---
+++ src/main/java/typecasting/FountainPen.java
@@ -0,0 +1,10 @@
+package typecasting;
+
+public class FountainPen extends Pen{
+    public void penColour(String colour)
+    {
+        System.out.println(colour+ "colour of Fountain pen");
+    }
+
+
+}
