d9e85a48c5f406ccbc22c5bb70622fd3bfaef394
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-22 15:55:35 +0530

    practice programs

--- src/main/java/polymorphism/overloadingdrivecar/CarServiceApp.java
+++ src/main/java/polymorphism/overloadingdrivecar/CarServiceApp.java
@@ -1,13 +1,13 @@
-package polymorphism.overloadingdrivecar;
-
-public class CarServiceApp {
-    public static void main(String[] args) {
-        CarService cs= new CarService();
-        cs.drive(300);
-        cs.print();
-        cs.drive(150,2);
-        cs.print();
-        cs.drive(200,4,0);
-        cs.print();
-    }
-}
+//package polymorphism.overloadingdrivecar;
+//
+//public class CarServiceApp {
+//    public static void main(String[] args) {
+//        CarService cs= new CarService();
+//        cs.drive(300);
+//        cs.print();
+//        cs.drive(150,2);
+//        cs.print();
+//        cs.drive(200,4,0);
+//        cs.print();
+//    }
+//}
