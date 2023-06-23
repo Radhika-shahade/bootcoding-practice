1c9aa42ef0204bb638936d8ca9e50c6a71d53916
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-23 23:20:38 +0530

    non primitive casting

---
+++ src/main/java/casting/nonpremitivecasting/Child.java
@@ -0,0 +1,10 @@
+package casting.nonpremitivecasting;
+
+public class Child extends Parent{
+    public void m1()
+    {
+        System.out.println("child class method is executing");
+    }
+
+
+}
