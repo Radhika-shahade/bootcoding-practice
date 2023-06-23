90c4f871e7ecbc97df9c2eeb6fbb1f749ccb80ec
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-23 19:47:50 +0530

    casting

---
+++ src/main/java/casting/watchexample/WristWatch.java
@@ -0,0 +1,9 @@
+package casting.watchexample;
+
+public class WristWatch extends Watch{
+    public void prize()
+    {
+        System.out.println("cost of wristwatch is starting from 500");
+    }
+
+}
