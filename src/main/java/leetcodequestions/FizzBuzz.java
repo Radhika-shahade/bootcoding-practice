22a409a5b76269b08e3b70d397150890485f4698
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-23 19:49:35 +0530

    leetcode question

---
+++ src/main/java/leetcodequestions/FizzBuzz.java
@@ -0,0 +1,38 @@
+package leetcodequestions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Que-412
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String>res=new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        {
+            if((i%3==0)&&(i%5==0))
+            {
+                res.add("FizBuzz");
+            }
+           else if(i%3==0)
+            {
+                res.add("Fizz");
+            }
+           else if(i%5==0)
+            {
+                res.add("Buzz");
+            }
+            else
+                res.add(i+" ");
+
+        }
+
+        System.out.println(res);
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fb= new FizzBuzz();
+        fb.fizzBuzz(3);
+    }
+}
