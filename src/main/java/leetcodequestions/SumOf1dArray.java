368deda867090c30f417fd139ab525810557f97f
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-23 16:24:20 +0530

    leetcode practice

--- src/main/java/leetcodequestions/SumOf1dArray.java
+++ src/main/java/leetcodequestions/SumOf1dArray.java
@@ -1,21 +1,24 @@
 package leetcodequestions;
-// Running Sum of 1d Array
+// 1480.Running Sum of 1d Array
 public class SumOf1dArray {
     public int [] getSumOfArray(int arr[])
     {
-        int result=0;
-
-        for(int i= 0; i<arr.length;i++)
+        int result[]= new int[arr.length];
+         result[0]=arr[0];
+        for(int i= 1; i<arr.length;i++)
         {
-            result= result+arr[i];
-            System.out.println(result);
+            result[i]= result[i-1]+arr[i];
         }
-        return arr;
+        return result;
     }
 
     public static void main(String[] args) {
-        int arr[]={3,1,2,10,1};
+        int arr[]={1,2,3,4};
         SumOf1dArray result= new SumOf1dArray();
-        result.getSumOfArray(arr);
+        int res[]=result.getSumOfArray(arr);
+        for(int i=0;i<res.length;i++)
+        {
+            System.out.println(res[i]);
+        }
     }
 }
