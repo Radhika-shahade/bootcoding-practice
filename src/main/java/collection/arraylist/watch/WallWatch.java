1afcae8b9631ebfa7d581092df8c31ca5567ec36
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-25 02:07:48 +0530

    arraylist of objects

---
+++ src/main/java/collection/arraylist/watch/WallWatch.java
@@ -0,0 +1,15 @@
+package collection.arraylist.watch;
+
+public class WallWatch extends Watch{
+    private int cost;
+
+    public WallWatch(String companyName, int cost) {
+        super(companyName);
+        this.cost=cost;
+    }
+    public void display()
+    {
+        System.out.println("company name: "+ companyName + "cost is "+cost );
+    }
+
+}
