b19dfcf876c35785f592c3e4f64661937b50661f
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-25 01:44:33 +0530

    array of object

--- src/main/java/arryofobjects/Shape/Main.java
+++ src/main/java/arryofobjects/Shape/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Shapes[] obj= new Shapes[5];
         obj[0]=new Shapes("black");
-        obj[1]=new Circle("yello",2.3);
+        obj[1]=new Circle("yellow",2.3);
         obj[2]=new Rectangle("Blue",2,5);
         obj[3]=new Circle("green",3.5);
         obj[4]=new Rectangle("white",3,6);
