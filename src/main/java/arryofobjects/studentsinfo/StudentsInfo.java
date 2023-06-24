d220b5090e4b0e5d63c10f94761a6cf8c7917570
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-25 00:22:17 +0530

    array of object

---
+++ src/main/java/arryofobjects/studentsinfo/StudentsInfo.java
@@ -0,0 +1,12 @@
+package arryofobjects.studentsinfo;
+
+public class StudentsInfo {
+     String name;
+     int age;
+    public StudentsInfo(String name, int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+}
