06b3a40442f849b62414cdbe7cf2364ef00a839e
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-25 01:43:19 +0530

    array of object

---
+++ src/main/java/arryofobjects/watches/WallWatch.java
@@ -0,0 +1,10 @@
+package arryofobjects.watches;
+
+public class WallWatch extends Watch{
+    private int cost;
+
+    public WallWatch(String companyName, int cost) {
+        super(companyName);
+        this.cost=cost;
+    }
+}
