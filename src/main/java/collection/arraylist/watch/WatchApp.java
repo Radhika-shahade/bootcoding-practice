a19d8a769f5f9de9e609756a383aad09bc3ae77b
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-25 02:08:18 +0530

    arraylist of objects

---
+++ src/main/java/collection/arraylist/watch/WatchApp.java
@@ -0,0 +1,18 @@
+package collection.arraylist.watch;
+
+
+import java.util.ArrayList;
+
+public class WatchApp {
+
+    public static void main(String[] args) {
+        ArrayList<Watch>obj= new ArrayList<>();
+     obj.add(0,new Watch("Titan"));
+     obj.add(1,new WallWatch("Sonata ",4000));
+     obj.add(2,new WristWatch("MI ","circular"));
+     for(int i=0;i<obj.size();i++)
+     {
+        obj.get(i).display();
+     }
+    }
+}
