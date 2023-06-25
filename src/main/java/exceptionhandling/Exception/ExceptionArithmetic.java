e347a1334388749cf7798f586088b699a87d5473
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-25 15:26:32 +0530

    Exception

---
+++ src/main/java/exceptionhandling/Exception/ExceptionArithmetic.java
@@ -0,0 +1,11 @@
+package exceptionhandling.Exception;
+
+public class ExceptionArithmetic {
+    public static void main(String[] args) {
+        int i=10;
+        int j=0;
+        int k=0;
+        k=i/j;
+        System.out.println(k);
+    }
+}
