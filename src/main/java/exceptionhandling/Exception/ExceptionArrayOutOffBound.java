6bed59cb09e0f21f1e0f768fafee96f3d6e51394
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-25 15:29:55 +0530

    Exception

---
+++ src/main/java/exceptionhandling/Exception/ExceptionArrayOutOffBound.java
@@ -0,0 +1,8 @@
+package exceptionhandling.Exception;
+
+public class ExceptionArrayOutOffBound {
+    public static void main(String[] args) {
+        int a[]={2,3,4,5};
+        System.out.println(a[0]);
+    }
+}
