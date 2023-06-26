9aafe1b5736aa11d10ddd99a984dc28c51f89170
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-26 22:52:57 +0530

    collection

---
+++ src/main/java/collection/arraylist/premitivedatatype/Test2.java
@@ -0,0 +1,15 @@
+package collection.arraylist.premitivedatatype;
+
+import java.util.ArrayList;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<String>obj= new ArrayList<>();
+        obj.add("mango");
+        obj.add("banana");
+        obj.add("grapes");
+        obj.add("pineapple");
+        obj.add("litchi");
+        System.out.println(obj);
+    }
+}
