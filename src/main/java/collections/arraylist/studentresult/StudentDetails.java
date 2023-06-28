4992eb50615c4bc5e386aa1e0fb9489587670781
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-28 22:54:00 +0530

    practice

---
+++ src/main/java/collections/arraylist/studentresult/StudentDetails.java
@@ -0,0 +1,43 @@
+package src.main.java.collections.arraylist.studentresult;
+
+public class StudentDetails {
+    private String name;
+    private int obtainMark;
+
+    private int totalMark;
+    private String subjectName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getObtainMark() {
+        return obtainMark;
+    }
+
+    public void setObtainMark(int obtainMark) {
+        this.obtainMark = obtainMark;
+    }
+
+    public int getTotalMark() {
+        return totalMark;
+    }
+
+    public void setTotalMark(int totalMark) {
+        this.totalMark = totalMark;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+
+}
