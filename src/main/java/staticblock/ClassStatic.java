a0ee30d15dac9b21fa7b66591d2cb3b3d092160e
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-30 22:32:13 +0530

    practice

---
+++ src/main/java/staticblock/ClassStatic.java
@@ -0,0 +1,16 @@
+package src.main.java.staticblock;
+
+public class ClassStatic {
+    static class Static{
+        public static void m1()
+        {
+            System.out.println("m1 method name is executing");
+        }
+        public static void main(String[] args) {
+            m1();
+
+        }
+    }
+
+
+}
