8f1145b07c1f819b02dde5edbf2f3b92d592963b
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-30 22:34:39 +0530

    practice

--- src/main/java/inheritance/CheckBalance.java
+++ src/main/java/inheritance/CheckBalance.java
@@ -1,23 +1,23 @@
-package inheritance;
-
-public class CheckBalance {
-
-    public int checkingBalance(int totalamount ,int moneywithdraw)
-    {
-        int balance= totalamount-moneywithdraw;
-        System.out.println("bank balance is:"+balance);
-        return balance;
-
-    }
-
-    public static void main(String[] args) {
-        Deposit dp= new Deposit();
-       int totalamount = dp.depositedamount(15000);
-        Withdraw wd= new Withdraw();
-       int  moneywithdraw = wd.withdrawAmount(4000);
-        CheckBalance cv= new CheckBalance();
-        cv.checkingBalance(totalamount,moneywithdraw );
-
-
-    }
-}
+//package inheritance;
+//
+//public class CheckBalance {
+//
+//    public int checkingBalance(int totalamount ,int moneywithdraw)
+//    {
+//        int balance= totalamount-moneywithdraw;
+//        System.out.println("bank balance is:"+balance);
+//        return balance;
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        Deposit dp= new Deposit();
+//       int totalamount = dp.depositedamount(15000);
+//        Withdraw wd= new Withdraw();
+//       int  moneywithdraw = wd.withdrawAmount(4000);
+//        CheckBalance cv= new CheckBalance();
+//        cv.checkingBalance(totalamount,moneywithdraw );
+//
+//
+//    }
+//}
