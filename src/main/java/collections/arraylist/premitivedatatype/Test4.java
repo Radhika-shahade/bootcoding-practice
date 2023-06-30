0573629d15790e49d1d2d0de42b04d9587883175
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-30 22:33:58 +0530

    practice

---
+++ src/main/java/collections/arraylist/premitivedatatype/Test4.java
@@ -0,0 +1,15 @@
+package collections.arraylist.premitivedatatype;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Test4 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the ");
+
+        ArrayList<String>sName= new ArrayList<>();
+        sName.add("");
+
+    }
+}
