847ca9986be6094788df7dfb39a726e54be55db3
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-30 22:33:35 +0530

    practice

---
+++ src/main/java/staticblock/Test.java
@@ -0,0 +1,10 @@
+package src.main.java.staticblock;
+// static blck will execute first
+public class Test {
+    static {
+        System.out.println("static lock is executing");
+    }
+    public static void main(String[] args) {
+
+    }
+}
