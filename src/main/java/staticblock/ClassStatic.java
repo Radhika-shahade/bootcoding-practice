0beb2a9dbf4903aa1135321b446b176a4fdef608
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-07 20:21:52 +0530

    practice

--- src/main/java/staticblock/ClassStatic.java
+++ src/main/java/staticblock/ClassStatic.java
@@ -1,12 +1,21 @@
 package src.main.java.staticblock;
 
 public class ClassStatic {
+    public void m2()
+    {
+        System.out.println("m2 is executing");
+    }
     static class Static{
+
+
         public static void m1()
         {
             System.out.println("m1 method name is executing");
         }
         public static void main(String[] args) {
+            ClassStatic obj= new ClassStatic();
+            obj.m2();
+
             m1();
 
         }
