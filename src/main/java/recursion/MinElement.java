ad0575d6f319eff06df026b9e14fe956b067caef
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-08 23:51:14 +0530

    Recursion

---
+++ src/main/java/recursion/MinElement.java
@@ -0,0 +1,28 @@
+package src.main.java.recursion;
+
+public class MinElement {
+    public int findMinimumElement(int []num,int min,int i)
+    {
+        if(i==num.length)
+        {
+            return min;
+        }
+        if(min>num[i])
+        {
+            min=num[i];
+
+        }
+        //Math.min(x,y);
+        //min=Math.min(min,a[i])
+        return findMinimumElement(num,min,++i);
+    }
+
+    public static void main(String[] args) {
+        MinElement obj= new MinElement();
+        int arr[]={1,2,3,4};
+        System.out.println(obj.findMinimumElement(arr,2,0));
+
+
+    }
+
+}
