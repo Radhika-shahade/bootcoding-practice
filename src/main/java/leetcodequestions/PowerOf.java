68ef0bec15f1a81642bd711039faf65966313123
Radhika shahade <radhikashahade0804@gmail.com>
2023-08-27 18:24:07 +0530

    leetcode solution

---
+++ src/main/java/leetcodequestions/PowerOf.java
@@ -0,0 +1,17 @@
+package src.main.java.leetcodequestions;
+//231 leetCode solution
+public class PowerOf {
+    public static boolean isPowerOfTwo(int n)
+    {
+        if(n<=0)
+        {
+            return false;
+        }
+        return (n & (n-1))==0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(24));
+    }
+
+}
